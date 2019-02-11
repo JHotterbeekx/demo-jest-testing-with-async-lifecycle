b177523d7d2848ec79c0459fee62cdc75eb2c4ab
John Hotterbeekx <john.online@hotterbeekx.nl>
2019-02-11 20:34:07 +0100

    Built with async/await

--- src/components/DataDisplayer.js
+++ src/components/DataDisplayer.js
@@ -17,20 +17,19 @@ export default class DataDisplayer extends React.Component {
   // We use the componentDidMount to trigger the retrieval of the data once the component is
   // mounted. Which means the component first mounts with its default state and than triggers
   // this method so data is retrieved.
-  componentDidMount() {
+  async componentDidMount () {
     // We create a new instance of data retriever and call the retrieve method. In this
     // retrieve method we pass a so-called callback method as a parameter. This method will
     // be called inside the retrieve method. As you can see the method expects a title parameter
     // which it will set on the data property in the state and also setting the dataAvailable
     // property to true;
-    RetrieveData().then(title => {
-      if(title){
-        this.setState({
-          dataAvailable: true,
-          data: title
-        });
-      }
-    });
+    const title = await RetrieveData();
+    if(title){
+      this.setState({
+        dataAvailable: true,
+        data: title
+      });
+    }
   }
 
   // This render method will initially render the text 'Data not available', because in the 
--- src/components/DataRetriever.js
+++ src/components/DataRetriever.js
@@ -3,10 +3,8 @@
 // This demo method calls an open API, then translates the response to JSON. Once that is done
 // it calls the passed in callbackMethod with the title property as parameter. So when the API
 // gives us { title: 'myTitle' }, the code will perform callbackMethod('myTitle')
-export default () => {
-  return fetch("https://jsonplaceholder.typicode.com/todos/1")
-    .then(response => {
-      return response.json();
-    })
-    .then(responseJson => responseJson.title);
+export default async() => {
+  const todoData = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const todoDataJson = await todoData.json();
+  return todoDataJson.title;
 }
